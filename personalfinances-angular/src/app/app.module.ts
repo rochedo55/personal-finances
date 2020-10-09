86a02c0891963fcb5f9f8bbe2cf0fc24ac722603
gabriel <rochasouzagabriel55@gmail.com>
2020-10-09 20:04:43 -0300

    componentes header sidemenu e minicard criados usando ng g c nomedocomponente

--- personalfinances-angular/src/app/app.module.ts
+++ personalfinances-angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { MiniCardComponent } from './mini-card/mini-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    MiniCardComponent
   ],
   imports: [
     BrowserModule
